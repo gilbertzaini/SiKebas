659c7fad4304202cbb1d8ff24d9b304972d390a6
gilbertzaini <gilbertzaini03@gmail.com>
2023-08-30 21:39:58 +0700

    updated dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,77 +1,128 @@
 @extends('layouts.master')
 
-@section('additionalHeadContent')
-<style>
-  .icons {
-    width: 60px  !important;
-    height:  60px !important;
-  }
-</style>
-@endsection
-
 @section('content')
-  <!-- Section -->
-  <section class="jumbotron">
-    <div class="container mt-5">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          <h1 class="text-white text-center my-5 ">Dashboard</h1>
-          <div class="col mt-5">
-            <img src="../assets/add-user.png" class="img icons">
-            <div id="text1">
-              <h1><strong class="text-white">Total Pengurus</strong></h1>
+<section>
+  <div class="jumbotron mt-3">
+    <div class="mt-2 d-flex justify-center flex-column justify-content-center">
+      <div class="d-flex justify-content-center align-items-center mb-5">
+        <img src="{{asset('assets/iPhone_14_Pro_Max_-_1-removebg-preview.png')}}" style="width: 180px; height: auto !important;">
+      </div>
+      <div class="d-md-flex justify-content-sm-center align-items-sm-center flex-lg-row flex-md-column">
+        <div class="d-flex mb-4">
+          <div class="mx-2">
+            <img src="{{asset('assets/add-user.png')}}" class="img" style="width: 80px; height: auto !important;">
+          </div>
+          <div id="text1">
+            <h3 class="my-3"><strong class="text-white">Total Pengurus</strong></h1>
               <!-- <p class="text-white">Bulan ini</p> -->
-              <div class="d-flex container mt-4 mx-2">
-                <i class="gg-trending"></i>
-                <p>{{$pengurus->count()}}</p>
-              </div>
-            </div>
-          </div>
-          <div class="col mt-5">
-            <div class="container-fluid">
-              <img src="../assets/group.png" class="img icons">
-              <div id="text1">
-                <h1><strong class="text-white">Total Nasabah</strong></h1>
-                <!-- <p class="text-white">Bulan ini</p> -->
-                <div class="d-flex container mt-4 mx-2">
+              <div class="d-flex mx-2">
+                <div class="mt-2">
                   <i class="gg-trending"></i>
-                  <p>{{$nasabah->count()}}</p>
                 </div>
+                <p style="font-size: 18px;">{{$pengurus->count()}} Orang</p>
               </div>
-            </div>
           </div>
-          <div class="col mt-5">
-            <div class="container-fluid">
-              <img src="../assets/shopping-online.png" class="img icons" id="img1">
-              <div id="text1">
-                <h1><strong class="text-white">Total Transaksi</strong></h1>
-                <!-- <p class="text-white">Bulan ini</p> -->
-                <div class="d-flex container mt-4 mx-2">
+        </div>
+        <div class="d-flex mb-4">
+          <div class="mx-2">
+            <img src="{{asset('assets/group.png')}}" class="img" style="width: 80px; height: auto !important;">
+          </div>
+          <div id="text1">
+            <h3 class="my-3"><strong class="text-white">Total Nasabah</strong></h1>
+              <!-- <p class="text-white">Bulan ini</p> -->
+              <div class="d-flex mx-2">
+                <div class="mt-2">
                   <i class="gg-trending"></i>
-                  <p>{{$transaction}}</p>
                 </div>
+                <p style="font-size: 18px;">{{$nasabah->count()}} Orang</p>
               </div>
           </div>
+        </div>
+        <div class="d-flex mb-4">
+          <div class="mx-2">
+            <img src="{{asset('assets/shopping-online.png')}}" class="img" style="width: 80px; height: auto !important;">
           </div>
-          <div class="col mt-5">
-            <div class="container-fluid">
-              <img src="../assets/dollar.png" class="img icons" id="img2">
-              <div id="text1">
-                <h1><strong class="text-white">Total Saldo</strong></h1>
-                <!-- <p class="text-white">Bulan ini</p> -->
-                <div class="d-flex container mt-4 mx-2">
+          <div id="text1">
+            <h3 class="my-3"><strong class="text-white">Total Transaksi</strong></h1>
+              <!-- <p class="text-white">Bulan ini</p> -->
+              <div class="d-flex mx-2">
+                <div class="mt-2">
                   <i class="gg-trending"></i>
-                  <p>Rp. {{ number_format($saldo, 2, ',', '.') }}</p>
                 </div>
+                <p style="font-size: 18px;">{{$transaction}}</p>
               </div>
-            </div>
+          </div>
+        </div>
+        <div class="d-flex mb-4">
+          <div class="mx-2">
+            <img src="{{asset('assets/dollar.png')}}" class="img" style="width: 80px; height: auto !important;">
+          </div>
+          <div id="text1">
+            <h3 class="my-3"><strong class="text-white">Total Saldo</strong></h1>
+              <!-- <p class="text-white">Bulan ini</p> -->
+              <div class="d-flex mx-2">
+                <div class="mt-2">
+                  <i class="gg-trending"></i>
+                </div>
+                <p style="font-size: 18px;">Rp. {{ number_format($saldo, 2, ',', '.') }}</p>
+              </div>
           </div>
         </div>
       </div>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffff" fill-opacity="1" d="M0,96L60,85.3C120,75,240,53,360,53.3C480,53,600,75,720,112C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-  </section>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-    crossorigin="anonymous"></script>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffff" fill-opacity="1" d="M0,96L60,85.3C120,75,240,53,360,53.3C480,53,600,75,720,112C840,149,960,203,1080,213.3C1200,224,1320,192,1380,176L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+    </svg>
+  </div>
+</section>
+
+<main>
+  <div class="text-center container-gallery-dashboard fw-bold mb-3">
+    <h1 id="gallery-dashboard">GALERI</h1>
+  </div>
+  <div id="carouselExampleDark" class="carousel carousel-dark slide px-5">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100 h-100" alt="...">
+        <div class="carousel-caption d-none d-md-block text-white">
+          <h5>Gambar 1</h5>
+          <p>...</p>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100 h-100" alt="...">
+        <div class="carousel-caption d-none d-md-block text-white">
+          <h5>Gambar 2</h5>
+          <p>...</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100 h-100" alt="...">
+        <div class="carousel-caption d-none d-md-block text-white">
+          <h5>Gambar 3</h5>
+          <p>...</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</main>
+<footer class="bg-dark text-center text-white">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-white" href="#">Universitas Multimedia Nusantara</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script>
+  const menu = () => document.body.classList.toggle("open");
+</script>
 @endsection
