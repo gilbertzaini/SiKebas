7a70d721779d6cd9081521ef7d65d37db04cd94b
gilbertzaini <gilbertzaini03@gmail.com>
2023-08-31 20:37:05 +0700

    transactions date filter done, laporan date filter is now a component

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -114,15 +114,4 @@
     </button>
   </div>
 </main>
-<footer class="bg-dark text-center text-white">
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2023 Copyright:
-    <a class="text-white" href="#">Universitas Multimedia Nusantara</a>
-  </div>
-  <!-- Copyright -->
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<script>
-  const menu = () => document.body.classList.toggle("open");
-</script>
 @endsection
